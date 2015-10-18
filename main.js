d3a5d80752dee2a962b87e76395fb77a00079d46
Justin Miller <superiorjt@gmail.com>
2015-10-17 22:36:52 -0400

    added json parsing

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var bcrypt = require('bcrypt');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+
+app.post('/api/register', function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
+});
 
 server.listen(3000, function() {
     var host = server.address().address;
