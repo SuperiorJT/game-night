e21fe2e0923fe9f73f394eb5b851e364b5952ca8
Justin Miller <superiorjt@gmail.com>
2015-10-17 22:03:03 -0400

    public static directory set up

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+app.use(express.static('public'));
 
 server.listen(3000, function() {
     var host = server.address().address;
